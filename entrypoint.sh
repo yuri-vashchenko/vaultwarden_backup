d47892e8bfed13daf9c501320cbf261bdfedcb52
jmqm <jmqm@outlook.com>
2021-07-15 13:09:32 -0500

    Fix delete script overwriting backup script
    - `delete` script was overwriting `backup script`, so only `delete` ever ran; this is fixed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,13 +14,13 @@ fi
 # Create cron jobs.
 if [ "$(id -u)" -eq 0 ] && [ "$(grep -c "$BACKUP_CMD" "$CRONFILE")" -eq 0 ]; then
     # Add backup script to cron jobs.
-    echo "$CRON_TIME $BACKUP_CMD >> $LOGS_FILE 2>&1" | crontab -
+    (crontab -l 2>/dev/null; echo "$CRON_TIME $BACKUP_CMD >> $LOGS_FILE 2>&1") | crontab -
     echo "[INFO] Added backup script to cron jobs."
 
     # Check if $DELETE_AFTER is not null and is greater than 0.
     # If so, add it to cron jobs.
     if [ -n "$DELETE_AFTER" ] && [[ "$DELETE_AFTER" -gt 0 ]]; then
-        echo "$CRON_TIME $DELETE_CMD >> $LOGS_FILE 2>&1" | crontab -
+        (crontab -l 2>/dev/null; echo "$CRON_TIME $DELETE_CMD >> $LOGS_FILE 2>&1") | crontab -
         echo "[INFO] Added delete script to cron jobs."
     fi
 fi
